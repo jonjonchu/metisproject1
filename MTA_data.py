ea0466b9dfa457b9debbe159b94becd44c176d68
Jonathan Chu <jonathan.chu@uni-oldenburg.de>
2020-03-30 16:28:10 -0700

    added two text files and exploratory nb

---
+++ MTA_data.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 30 15:13:22 2020
+
+@author: kibbles
+"""
+import pandas as pd
+
+
+filename = 'turnstile_200328.txt'
+
+turnstile = pd.read_csv(filename)
